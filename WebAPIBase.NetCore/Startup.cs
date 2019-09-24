16f9199c589f5d4d2bdb2f5b62bf1a9d95638dbf
Thiago Loureiro <thiagoguaru@me.com>
2019-09-24 21:55:36 +0800

    .NET Core 3.0 Support

--- WebAPIBase.NetCore/Startup.cs
+++ WebAPIBase.NetCore/Startup.cs
@@ -1,10 +1,10 @@
 using Data.DapperORM.Class;
 using Data.DapperORM.Interface;
+using EasyMemoryCache;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -13,7 +13,8 @@ using Service.Class;
 using Service.Interface;
 using System;
 using System.Collections.Generic;
-using EasyMemoryCache;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace WebAPIBase.NetCore
 {
@@ -62,10 +63,9 @@ namespace WebAPIBase.NetCore
                     .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
                     .RequireAuthenticatedUser().Build());
             });
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "My API", Version = "v1" });
-            });
+
+            services.AddMvcCore()
+                .AddApiExplorer();
 
             services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
             {
@@ -91,35 +91,39 @@ namespace WebAPIBase.NetCore
                     policy => policy.RequireClaim("MembershipId"));
             });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIBase API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseStaticFiles();
-            app.UseAuthentication();
-            app.UseMvcWithDefaultRoute();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseCors();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
 
             app.UseSwagger();
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-            });
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseMvc();
-
-            app.UseDefaultFiles(new DefaultFilesOptions
-            {
-                DefaultFileNames = new
-                    List<string> { "index.html" }
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ForSignAPIClient v1");
             });
         }
     }
